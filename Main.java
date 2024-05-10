c6a09e0206fbf9f0620e7e980e357584306f0868
Esteban Molina Mejia <emolinam@eafit.edu.co>
2024-05-10 17:04:40 -0500

    clase main

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        imprimirnombre();
+    }
+    public static void imprimirnombre(){
+        System.out.println("Assingnment 3 by <Esteban Molina>");
+    }
+}
